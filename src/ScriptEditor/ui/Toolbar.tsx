8c7b1cc005b7360102c1de9a27017c661a4e5be6
branchtheory <robert.g.silverstone@gmail.com~>
2026-09-08 21:59:41 +0100

    isRunning additions

--- src/ScriptEditor/ui/Toolbar.tsx
+++ src/ScriptEditor/ui/Toolbar.tsx
@@ -89,7 +89,9 @@ export function Toolbar({ editor, onSave, onRun, onBeautify }: IProps) {
         </Button>
         <Tooltip title={parseKeyCombinationsToString(CurrentKeyBindings[ScriptEditorAction.Save])}>
           <Button
-            color={ITutorial.currStep === iTutorialSteps.ScriptEditorEditAndSave ? "info" : "primary"}
+            color={
+              ITutorial.isRunning && ITutorial.currStep === iTutorialSteps.ScriptEditorEditAndSave ? "info" : "primary"
+            }
             onClick={() => {
               onSave().catch((error) => console.error(error));
             }}
